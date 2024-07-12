6c22fdb6f3c1dba6439bde72f68fb12a94aa5bbe
Tomasz <pengwius@protonmail.ch>
2024-07-12 18:23:58 +0200

    bump up version

--- app/src/main/java/io/github/freewulkanowy/ui/modules/dashboard/viewholders/AdminMessageViewHolder.kt
+++ app/src/main/java/io/github/freewulkanowy/ui/modules/dashboard/viewholders/AdminMessageViewHolder.kt
@@ -19,8 +19,6 @@ class AdminMessageViewHolder(
     fun bind(item: AdminMessage?, showPanicButton: Boolean = false) {
         item ?: return
 
-        item.isOkVisible = true
-
         val context = binding.root.context
         val (backgroundColor, textColor) = when (item.priority) {
             "HIGH" -> {
